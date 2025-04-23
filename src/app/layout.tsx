34816473a09cab6157aa160bd15b8acdf17f4510
Hassan Aziz <hassanaziz0012@gmail.com>
2025-04-23 10:54:24 +0500

    Remove unnecessary classes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`bg-white dark:bg-slate-900 dark:text-white transition-all duration-300`}>{children}</body>
+            <body className={``}>{children}</body>
         </html>
     );
 }
